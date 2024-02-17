52c849b40378cc60a047f642bd470500a4c6a771
Sukomal07 <hypersukomal@gmail.com>
2024-02-17 21:18:35 +0530

    push to github

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import errorMiddleware from './middlewares/error.middleware.js'
+import dotenv from 'dotenv'
+
+import userRoutes from './routes/user.routes.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+//routes config
+app.use("/api/v1/user", userRoutes)
+
+app.all("*", (req, res) => {
+    res.status(404).json({
+        status: 404,
+        success: false,
+        message: "!Oops page not found"
+    })
+})
+
+app.use(errorMiddleware)
+export default app
